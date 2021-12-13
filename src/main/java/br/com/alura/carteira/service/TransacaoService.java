a6bfa3313bdafa1358fea423e4ee42ad641ab1c7
Jonilson Sousa <jonilsonds9@gmail.com>
2021-12-13 16:10:25 -0300

    Arrumando service para pegar usuario

--- src/main/java/br/com/alura/carteira/service/TransacaoService.java
+++ src/main/java/br/com/alura/carteira/service/TransacaoService.java
@@ -3,7 +3,9 @@ package br.com.alura.carteira.service;
 import br.com.alura.carteira.dto.TransacaoDto;
 import br.com.alura.carteira.dto.TransacaoFormDto;
 import br.com.alura.carteira.modelo.Transacao;
+import br.com.alura.carteira.modelo.Usuario;
 import br.com.alura.carteira.repository.TransacaoRepository;
+import br.com.alura.carteira.repository.UsuarioRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
@@ -20,6 +22,7 @@ import java.util.stream.Collectors;
 public class TransacaoService {
 
     private final TransacaoRepository transacaoRepository;
+    private final UsuarioRepository usuarioRepository;
     private final ModelMapper modelMapper = new ModelMapper();
 
     public Page<TransacaoDto> listar(Pageable paginacao) {
@@ -29,8 +32,12 @@ public class TransacaoService {
 
     @Transactional
     public TransacaoDto cadastrar(TransacaoFormDto dto) {
+        Long usuarioId = dto.getUsuarioId();
+        Usuario usuario = usuarioRepository.getById(usuarioId);
+
         Transacao transacao = modelMapper.map(dto, Transacao.class);
         transacao.setId(null);
+        transacao.setUsuario(usuario);
         transacaoRepository.save(transacao);
         return modelMapper.map(transacao, TransacaoDto.class);
     }
